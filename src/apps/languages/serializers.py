525be08523aecd34d52bfeb778815d179cefbdaf
Ciwy <c.walter@afk-dev.de>
2023-12-07 17:23:51 +0100

    add: simple serializers

---
+++ src/apps/languages/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import Serializer
+
+class LanguageViewSerializer(Serializer):
+    """  """
+    fields = '__all__'
+
+
+class LanguageCreateSerializer(Serializer):
+    """  """
+    fields = '__all__'
+
