c7c4399571d1ba820e5fa716cf08de4d29b62ec6
Ciwy <c.walter@afk-dev.de>
2023-12-17 19:14:57 +0100

    rename: blueprint model

--- src/apps/blueprints/models.py
+++ src/apps/blueprints/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class ProjectBlueprint(models.Model):
+class Blueprint(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class BlueprintLanguageWeight(models.Model):
-    blueprint = models.ForeignKey(ProjectBlueprint, on_delete=models.CASCADE)
+    blueprint = models.ForeignKey(Blueprint, on_delete=models.CASCADE)
     language = models.ForeignKey('languages.Language', on_delete=models.CASCADE)
     weight = models.DecimalField(max_digits=6, decimal_places=6)
 
