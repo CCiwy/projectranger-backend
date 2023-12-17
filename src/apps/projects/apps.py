bc21d28818410c23921613e8d41119fa27507492
Ciwy <c.walter@afk-dev.de>
2023-12-17 19:13:35 +0100

    fix incorrect app labels

--- src/apps/projects/apps.py
+++ src/apps/projects/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ProjectsConfig(AppConfig):
+    name = 'apps.projects'
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.apps.projects'
