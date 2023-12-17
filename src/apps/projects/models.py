ff1daef18e8a2ce06f600476bd55f1433ca21ddd
Ciwy <c.walter@afk-dev.de>
2023-12-17 18:29:54 +0100

    refactor project related models

--- src/apps/projects/models.py
+++ src/apps/projects/models.py
@@ -11,31 +11,19 @@ class Project(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     github_url = models.URLField(blank=True)
 
-
-class ProjectNotes(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    note = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-class ProjectIdea(models.Model):
-    """ Ideas for projects: Title, description, and weights to languages and frameworks + skill levels,
-
-    """
-
-    title = models.CharField(max_length=100)
+class ProjectEntity(models.Model):
+    name = models.CharField(max_length=100)
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ProjectNotes(ProjectEntity):
+    entity_type = 'note'
+
+class ProjectTodos(ProjectEntity):
+    entity_type = 'todo'
 
 
-class ProjectLanguage(models.Model):
-    projectIdea = models.ForeignKey(ProjectIdea, on_delete=models.CASCADE)
-    language = models.ForeignKey('languages.Language', on_delete=models.CASCADE)
+class ProjectIdea(ProjectEntity):
+    entity_type = 'idea'
 
-    weight = models.DecimalField(max_digits=6, decimal_places=6)
-
-
-class ProjectSkill(models.Model):
-    projectIdea = models.ForeignKey(ProjectIdea, on_delete=models.CASCADE)
-    skill = models.ForeignKey('skills.Skill', on_delete=models.CASCADE) # todo: add skills app!
-
-    weight = models.DecimalField(max_digits=6, decimal_places=6)
