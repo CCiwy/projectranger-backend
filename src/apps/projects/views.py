1a9b316ec3487daca20138a8ae61867f5b995fc7
Ciwy <c.walter@afk-dev.de>
2023-12-17 19:29:07 +0100

    fix typos

--- src/apps/projects/views.py
+++ src/apps/projects/views.py
@@ -2,10 +2,12 @@ from decimal import Decimal
 from rest_framework import viewsets
 
 from .serializers import ProjectSerializer
-
+from .models import Project, ProjectNotes
 
 class ProjectViewSet(viewsets.ModelViewSet):
     """ todo: add logic to return different serializers depending on ownersship """
+    
+    queryset = Project.objects.all()
 
     def get_queryset(self):
         return Project.objects.all()
@@ -17,6 +19,7 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
 class ProjectNotesViewSet(viewsets.ModelViewSet):
 
+    queryset = ProjectNotes.objects.all()
 
     def get_queryset(self):
         return ProjectNotes.objects.filter(project=self.kwargs['project_id'])
