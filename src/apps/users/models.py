66f77c9045f6ee210988dabb9947c9887cf54980
Ciwy <c.walter@afk-dev.de>
2024-01-07 17:30:11 +0100

    add: user app/model

---
+++ src/apps/users/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=32, null=False)
+    bio = models.CharField(max_length=255)
+    is_public = models.BooleanField(default=False)
+
