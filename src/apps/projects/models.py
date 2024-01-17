b1f05e0738d07b7ce9478f89460ad69bb5b40638
Ciwy <c.walter@afk-dev.de>
2024-01-17 14:23:07 +0100

    add user relation to project model

--- src/apps/projects/models.py
+++ src/apps/projects/models.py
@@ -10,6 +10,7 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     github_url = models.URLField(blank=True)
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
 
 class ProjectEntity(models.Model):
     name = models.CharField(max_length=100)
