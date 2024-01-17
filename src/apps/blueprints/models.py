f91141d5f25fb10d857820bce4b40dbff67504d8
Ciwy <c.walter@afk-dev.de>
2024-01-17 14:08:53 +0100

    add: models and migrations for all apps

--- src/apps/blueprints/models.py
+++ src/apps/blueprints/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from apps.languages.models import Language
+
 
 class Blueprint(models.Model):
     name = models.CharField(max_length=100)
@@ -7,8 +9,9 @@ class Blueprint(models.Model):
 
 
 class BlueprintLanguageWeight(models.Model):
+    """ Weigth for Blueprint and Language """
     blueprint = models.ForeignKey(Blueprint, on_delete=models.CASCADE)
-    language = models.ForeignKey('languages.Language', on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
     weight = models.DecimalField(max_digits=6, decimal_places=6)
 
 
--- src/apps/languages/models.py
+++ src/apps/languages/models.py
@@ -8,8 +8,3 @@ class Language(models.Model):
     paradigm = models.CharField(max_length=24)
 
 
-class BlueprintLanguageWeight(models.Model):
-    """ Weigth for Blueprint and Language """
-    blueprint = models.ForeignKey('blueprints.Blueprint', on_delete=models.CASCADE)
-    language = models.ForeignKey('languages.Language', on_delete=models.CASCADE)
-    weight = models.DecimalField(max_digits=6, decimal_places=6)
