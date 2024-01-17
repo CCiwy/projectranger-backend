a8010b87c6232765f2439ca31965c79507bad21a
Ciwy <c.walter@afk-dev.de>
2024-01-17 14:33:36 +0100

    test: more test for project permissions

--- src/apps/projects/tests.py
+++ src/apps/projects/tests.py
@@ -6,53 +6,42 @@ from apps.users.models import CustomUser
 from apps.projects.models import Project
 
 
-
-
-
 class ProjectTests(APITestCase):
     """
     router.register(r'projects', ProjectViewSet)
     router.register(r'project_notes', ProjectNotesViewSet)
     """
-    
+
     @classmethod
     def setUpTestData(cls):
         cls.user = CustomUser.objects.create_user(
-            username='test_user',
-            password='test_password'
-            )
+            username="test_user", password="test_password"
+        )
 
         cls.project = Project.objects.create(
-            title='Test Project',
-            description='Test Project Description',
-            user=cls.user
+            title="Test Project", description="Test Project Description", user=cls.user
         )
- 
 
     def test_projects_unauthenticated_cant_view(self):
-        url = reverse('projects-list')
+        url = reverse("projects-list")
         response = self.client.get(url)
+
         self.assertNotEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertNotEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_project_authenticated_can_view(self):
+        self.client.force_authenticate(user=self.user)
+        url = reverse("projects-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_projects_only_owner_can_view(self):
+        second_user = CustomUser.objects.create_user(
+            username="second_user", email="second@user.com", password="second_password"
+        )
+        self.client.force_authenticate(user=second_user)
+        url = reverse("projects-list")
+        response = self.client.get(url)
 
-"""
-
-    def test_get_project_by_id(self):
-        url = reverse('project-detail', kwargs={'pk': self.project.id})
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, 200)
-
-
-    def test_create_project(self):
-        url = reverse('project-list')
-        data = {
-            'title': 'Test Project 2',
-            'description': 'This is also a test project'
-        }
-
-
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, 201)
-"""
+        self.assertNotEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertNotEqual(response.status_code, status.HTTP_200_OK)
